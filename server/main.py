00582c9311f9e796b0deb2c725866e48db5e3402
Aashihem <aashi.hemnani22@spit.ac.in>
2025-05-14 13:19:28 +0530

    made changes

--- server/main.py
+++ server/main.py
@@ -16,8 +16,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Update with specific origins if needed
-    allow_origins=["*"  # Ngrok URL
-],
+    # allow_origins=["https://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
